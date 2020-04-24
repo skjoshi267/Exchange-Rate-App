95fc23551dc463e793d23d93ec8499ca5a9c9895
skjoshi267 <sk.joshi267@gmail.com>
2020-04-24 20:33:39 +0530

    Changes in Configuration

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
+import os
+
 class Configuration(object):
     SECRET_KEY = 'Password@123'
-    FLASK_ENV = 'development'
-    FLASK_APP = 'exchange_rate.py'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost/exchange_rate'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:admin@localhost/exchange_rate'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
